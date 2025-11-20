3ad567c46a204df6149830bc9a2fc2e480502a5c
eziraa <ezratgab@gmail.com>
2025-11-20 23:00:24 +0300

    fix dehydration issues

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@ import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 export default async function Home() {
-  const token = (await cookies()).get("auth_token")?.value
-
+  const cookieStore = await cookies()
+  const token = cookieStore.get("auth_token")?.value
 
   if (!token) {
-    console.log("No auth token found, redirecting to login")
     redirect("/auth/login")
   }
 
   redirect("/dashboard")
-
 }
